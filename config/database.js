40e3c672c25042474d2337c47c7d93d682324d94
pushpanjali <pushpanjalikalangi@gmail.com>
2016-10-03 15:39:35 +0530

    checking images

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
-	'url' : 'mongodb://admin:admin@ds035846.mlab.com:35846/leavedb'
-//	'url':'mongodb://localhost:27017/leavedb'
+//	'url' : 'mongodb://admin:admin@ds035846.mlab.com:35846/leavedb'
+	'url':'mongodb://localhost:27017/leavedb'
  // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
